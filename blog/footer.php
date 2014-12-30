4fc2da2d3a693dcb8603c3dfa27c0af36d567b2c
Christina Chan <christinaavchan@gmail.com>
2014-12-29 22:38:19 -0500

    fixing footer

---
+++ blog/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Tonal
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="fullwidth-block site-footer" role="contentinfo">
+
+		<div class="site-info" style="font-size: 12px;">
+			<p><a href="http://christina.codes/blog/">HOME<a> | <a href="http://christina.codes/">ABOUT</a> | <a href="mailto:c.chan@queensu.ca">CONTACT</a><p>
+		</div><!-- .site-info -->
+
+	</footer><!-- #colophon .fullwidth-block .site-footer -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
