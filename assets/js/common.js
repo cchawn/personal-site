eea50cf28121d4e6276fb977d05932dbd461df3a
Christina Chan <cchan@brainstation.io>
2016-08-25 14:27:35 -0400

    added konami code disco 🙃

--- assets/js/common.js
+++ assets/js/common.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400){
 			var data = JSON.parse(request.responseText).data.image_url;
-			// console.log(data);
 			document.getElementById("giphy").innerHTML = '<img src = "'+data+'"  title="GIF via Giphy">';
 		} else {
 			console.log("API error");
@@ -18,8 +17,6 @@ $(document).ready(function () {
 		console.log('connection error');
 	};
 
-	// request.send();
-
 	$("#giphy-anchor").click(function(e) {
 		request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true);
 		request.send();
@@ -27,61 +24,10 @@ $(document).ready(function () {
 		e.preventDefault();
 	});
 
-	var $ken = $('.ken');
-	var $kenPos, $fireballPos;
-
-	function hadoken(){
-	  $ken.show(); // reveal ken with konami code
-	  $ken.addClass('hadoken'); 
-	  setTimeout(function() { $ken.removeClass('hadoken'); }, 500); 
-	  setTimeout(function() {
-	      var $fireball = $('<div/>', { class:'fireball' });
-	      $fireball.appendTo($ken);
-	              
-	      var isFireballColision = function(){ 
-	          return $fireballPos.left + 75 > $(window).width() ? true : false;
-	      };
-	  
-	      var explodeIfColision = setInterval(function(){
-	                  
-	          $fireballPos = $fireball.offset();
-	          //console.log('fireballInterval:',$fireballPos.left);
-	  
-	          if (isFireballColision()) {
-	              $fireball.addClass('explode').removeClass('moving').css('marginLeft','+=22px'); 
-	              clearInterval(explodeIfColision);
-	              setTimeout(function() { $fireball.remove(); }, 500); 
-	          }
-	  
-	      }, 50);
-	  
-	      setTimeout(function() { $fireball.addClass('moving'); }, 20);
-	              
-	      setTimeout(function() { 
-	          $fireball.remove(); 
-	          clearInterval(explodeIfColision);
-	      }, 3020);
-	  
-	  }, (250));
-	};
-
-	function explodeMessage(){
-		var $message = $('.message');
-		var x, y;
-		var max = 2000;
-		$message.html($message.text().replace(/./g, "<span>$&</span>"));
-		$message.children().each(function(){
-			x = (Math.random() * max) - max/2;
-			y = (Math.random() * max) - max/2;
-			$(this).css({
-				'transform' : 'translate(' + x + 'px ,' + y +'px)'
-			});
-		});
-	};
-
-	var easter_egg = new Konami(function() {
-		hadoken();
-		explodeMessage();
-		setTimeout(function() {$('.logo').addClass('spin');}, 20);
-	});
+	var EMOJIS = ['✨','✌︎','🌚','♕','⚡️','☹','💻'];
+	var ind = 1;
+	setInterval(function(){ 
+		$('#emoji').html(EMOJIS[ind]);
+		ind = (ind +  1) % EMOJIS.length;
+	}, 250);
 });
---
+++ assets/konami/konami.js
@@ -0,0 +1,125 @@
+$(document).ready(function(){
+	/**************************/
+	/* Konami Code Easter Egg */
+	/**************************/
+
+	var $ken = $('.ken');
+	var $kenPos, $fireballPos;
+
+	function hadoken(){
+	  $ken.show(); // reveal ken with konami code
+	  $ken.addClass('hadoken'); 
+	  setTimeout(function() { $ken.removeClass('hadoken'); }, 500); 
+	  setTimeout(function() {
+	      var $fireball = $('<div/>', { class:'fireball' });
+	      $fireball.appendTo($ken);
+	              
+	      var isFireballColision = function(){ 
+	          return $fireballPos.left + 75 > $(window).width() ? true : false;
+	      };
+	  
+	      var explodeIfColision = setInterval(function(){
+	                  
+	          $fireballPos = $fireball.offset();
+	          //console.log('fireballInterval:',$fireballPos.left);
+	  
+	          if (isFireballColision()) {
+	              $fireball.addClass('explode').removeClass('moving').css('marginLeft','+=22px'); 
+	              clearInterval(explodeIfColision);
+	              setTimeout(function() { $fireball.remove(); }, 500); 
+	          }
+	  
+	      }, 50);
+	  
+	      setTimeout(function() { $fireball.addClass('moving'); }, 20);
+	              
+	      setTimeout(function() { 
+	          $fireball.remove(); 
+	          clearInterval(explodeIfColision);
+	      }, 3020);
+	  
+	  }, (250));
+	};
+
+	function explodeMessage(){
+		$('.logo').addClass('spin');
+		var $message = $('.message');
+		var x, y;
+		var max = 100;
+		$message.html($message.text().replace(/./g, '<span>$&</span>'));
+		$message.children().each(function(){
+			x = (Math.random() * max) - max/2;
+			y = (Math.random() * max) - max/2;
+			$(this).animate({
+				'top' : y +'vh',
+				'left' : x + 'vh',
+				'opacity' : 0
+			}, 2000, function(){
+				reassembleMessage($(this).text(), HIRE_ME_PLS, $(this));
+				$message.css({'left':'40%'});
+			});
+		});
+	};
+
+	var COUNT = {};
+	var HIRE_ME_PLS = 'hiremepls';
+	var E_COUNT = 0;
+	for (var i = 0; i < HIRE_ME_PLS.length; i++){ COUNT[HIRE_ME_PLS[i]] = true; }
+	
+	function reassembleMessage(char, str, element){
+		if (str.indexOf(char) != '-1'){
+			if (COUNT[char] || (char == 'e' && E_COUNT == 1)){
+				element.css({'position':'absolute'});
+				switch (char) {
+					case 'h':
+						element.animate({'top':'0px','left':'0','opacity':1}, 1000);
+						break;
+					case 'i':
+						element.animate({'top':'0px','left':'12px','opacity':1}, 1000);
+						break;
+					case 'r':
+						element.animate({'top':'0px','left':'17px','opacity':1}, 1000);
+						break;
+					case 'e':
+						if (E_COUNT == 0){
+							E_COUNT = 1;
+							element.animate({'top':'0px','left':'25px','opacity':1}, 1000);
+						}else if (E_COUNT == 1){
+							E_COUNT = 2;
+							element.animate({'top':'0px','left':'70px','opacity':1}, 1000);
+						}
+						break;
+					case 'm':
+						element.animate({'top':'0px','left':'50px','opacity':1}, 1000);
+						break;
+					case 'p':
+						element.animate({'top':'0px', 'left':'90px','opacity':1}, 1000);
+						break;
+					case 'l':
+						element.animate({'top':'0px','left':'105px','opacity':1}, 1000);
+						break;
+					case 's':
+						element.animate({'top':'0px','left':'111px','opacity':1}, 1000);
+						break;
+					default:
+						// do nothing;
+				}
+				COUNT[char] = false;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function disco(){
+		$('body').addClass('disco');
+		$('.disco-ball').slideDown('slow');
+		$('.disco-ball').append('<audio id="audio" src="./assets/konami/take-on-me.m4a" autoplay></audio>');
+	}
+
+	var easter_egg = new Konami(function() {
+		hadoken();
+		explodeMessage();
+		if (!$('body').hasClass('disco')) setTimeout(function() { disco(); }, 2000);
+	});
+});
