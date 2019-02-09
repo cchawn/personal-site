2ead4fe847a0b4b122f5522a4685c6bdc9115bb8
Christina Chan <6462356+cchawn@users.noreply.github.com>
2019-02-08 21:21:26 -0500

    Webpack-ify everything (#2)
    * Set up webpack
    * Restructure and take advantage of webpack
    * Delete unnecessary files (remove konami code easter egg)

--- assets/js/common.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-  const EMOJIS = ['✨', '✌︎', '🌚', '♕', '⚡️', '☹', '💻'];
-  let ind = 1;
-  setInterval(() => {
-    $('#emoji').html(EMOJIS[ind]);
-    ind = (ind + 1) % EMOJIS.length;
-  }, 250);
-});
