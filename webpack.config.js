8ea805f980c313c86b9253d32e10d2e3de873ac2
Christina Chan <6462356+cchawn@users.noreply.github.com>
2019-02-08 21:11:57 -0500

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { url: false },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                autoprefixer({ browsers: ['> 1%', 'IE 10', 'Safari 9'] }),
+                cssnano,
+              ],
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(html|ico|png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
